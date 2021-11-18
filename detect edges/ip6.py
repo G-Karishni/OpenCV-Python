98202289e7e7076f48257a91466ef271aee5c103
G-Karishni <68386099+G-Karishni@users.noreply.github.com>
2021-11-18 11:56:42 +0530

    Add files via upload

---
+++ detect edges/ip6.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  2 17:05:37 2021
+
+@author: karish
+"""
+#detect edges with live cam
+
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+while(1):
+    _,frame = cap.read()
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    edges=cv2.Canny(gray,50,150,apertureSize=3)
+
+    cv2.imshow('edges',edges)
+    lines=cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength=500,maxLineGap=1)
+    for line in lines:
+        x1,y1,x2,y2=line[0]
+        cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),2)
+    
+    cv2.imshow('image',frame)
+    k=cv2.waitKey(0)& 0xFF
+    if k==27:
+        break
+
+cv2.destroyAllWindows()    
+
